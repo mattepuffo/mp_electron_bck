e48fb59fc43e5ed4b2b5f4093217b30b1c10ee67
mattepuffo <matteo.ferrone@hotmail.it>
2025-11-20 10:46:11 +0100

    bootstrap

--- preload.js
+++ preload.js
@@ -1,6 +1,12 @@
 const {contextBridge, ipcRenderer} = require('electron');
 
+const sharedConstants = {
+  APP_NAME: 'MP Backup',
+};
+
 contextBridge.exposeInMainWorld("db", {
   run: (sql, params) => ipcRenderer.invoke("db:run", sql, params),
   all: (sql, params) => ipcRenderer.invoke("db:all", sql, params)
 });
+
+contextBridge.exposeInMainWorld('appConstants', sharedConstants);
--- renderer/renderer.js
+++ renderer/renderer.js
@@ -1,3 +1,31 @@
+const appName = window.appConstants.APP_NAME;
+const contentArea = document.getElementById('main_content');
+
+function updateAppTitles() {
+  const elementsToUpdate = document.querySelectorAll('.app_title');
+
+  elementsToUpdate.forEach(element => {
+    element.textContent = appName;
+  });
+}
+
+
+//let opzioni = vec!["FTP", "SYNC", "LOG", "BACKUP DB", "PULISCI BCK", "ESCI"];
+document.addEventListener('DOMContentLoaded', () => {
+  updateAppTitles();
+});
+
+document.getElementById('link_ftp').addEventListener('click', async () => {
+  await console.log('ok');
+  await fetch('renderer/ftp.html')
+      .then(response => response.text())
+      .then(html => {
+        console.log(html);
+        contentArea.innerHTML = html;
+      })
+      .catch(err => console.error("Errore nel caricamento della pagina FTP:", err));
+});
+
 document.getElementById("test").addEventListener("click", async () => {
   await window.db.run(
       "INSERT INTO operation_log(operation, date) VALUES(?, datetime('now'))",
