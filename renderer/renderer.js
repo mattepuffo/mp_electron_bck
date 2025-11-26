0592a4d3d295f55c2cbe6e6bcd2ef00336a16b2b
mattepuffo <matteo.ferrone@hotmail.it>
2025-11-26 15:48:06 +0100

    mustache

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -17,31 +17,6 @@ document.getElementById("link_ftp").addEventListener("click", async () => {
 
     document.getElementById('main_content').innerHTML = await window.click.tpl("ftp.html");
 
-    // const populateTable = (data) => {
-    //   const tbody = document.querySelector('tbody');
-    //   tbody.innerHTML = '';
-    //
-    //   data.forEach(row => {
-    //     const tr = document.createElement('tr');
-    //     tr.innerHTML = `
-    //       <td>${row.name}</td>
-    //       <td>${row.host}</td>
-    //       <td>
-    //         <button type="button" class="btn btn-outline-warning btn_edit" data-name="${row.name}" data-host="${row.host}" data-username="${row.username}" data-password="${row.password}">
-    //           <i class="bi bi-pen"></i>
-    //         </button>
-    //         <button type="button" class="btn btn-outline-success btn_play" data-name="${row.name}">
-    //           <i class="bi bi-play-fill"></i>
-    //         </button>
-    //         <button type="button" class="btn btn-outline-danger btn_delete" data-name="${row.name}">
-    //           <i class="bi bi-trash"></i>
-    //         </button>
-    //       </td>
-    //     `;
-    //     tbody.appendChild(tr);
-    //   });
-    // };
-
     const populateTable = async (data) => {
       const tbody = document.querySelector('tbody');
 
@@ -145,3 +120,170 @@ document.getElementById("link_ftp").addEventListener("click", async () => {
     console.error("Errore:", error);
   }
 });
+
+document.getElementById("link_sync").addEventListener("click", async () => {
+
+  try {
+    document.getElementById('main_content').innerHTML = await window.click.tpl("sync.html");
+
+    // DRPDOWN TYPES
+    const types = ['ftp', 'local'];
+    const templateType = `
+      <option value="" selected>Seleziona un tipo</option>
+      {{#types}}
+      <option value="{{.}}">{{.}}</option>
+      {{/types}}
+    `;
+    document.getElementById('type').innerHTML = await window.mustache.render(templateType, {types: types});
+
+    // DRPDOWN SERVERS
+    let ftpServers = await window.db.all("SELECT * FROM ftp");
+    const templateFtp = `
+      <option value="" selected>Seleziona un server</option>
+      {{#ftpServers}}
+      <option value="{{name}}">{{name}}</option>
+      {{/ftpServers}}
+    `;
+    document.getElementById('server').innerHTML = await window.mustache.render(templateFtp, {ftpServers: ftpServers});
+
+    let rows = await window.db.all("SELECT * FROM sync");
+    const populateTable = async (data) => {
+      const tbody = document.querySelector('tbody');
+
+      const template = `
+        {{#rows}}
+          <tr>
+            <td>{{name}}</td>
+            <td>{{directory}}</td>
+            <td>{{server}}</td>
+            <td>{{path}}</td>
+            <td>{{type}}</td>
+            <td>
+              <button type="button"
+                      class="btn btn-outline-warning btn_edit"
+                      data-name="{{name}}" 
+                      data-directory="{{directory}}"
+                      data-server="{{server}}" 
+                      data-path="{{path}}" 
+                      data-type="{{type}}">
+                <i class="bi bi-pen"></i>
+              </button>
+              <button type="button" class="btn btn-outline-success btn_play" 
+                      data-name="{{name}}">
+                <i class="bi bi-play-fill"></i>
+              </button>
+              <button type="button" class="btn btn-outline-danger btn_delete" 
+                      data-name="{{name}}">
+                <i class="bi bi-trash"></i>
+              </button>
+            </td>
+          </tr>
+        {{/rows}}
+      `;
+
+      tbody.innerHTML = await window.mustache.render(template, {rows: data});
+    };
+
+    populateTable(rows);
+
+    document.querySelector('tbody').addEventListener('click', async (e) => {
+      const buttonEdit = e.target.closest('.btn_edit');
+      const buttonDelete = e.target.closest('.btn_delete');
+
+      if (buttonEdit) {
+        const rowData = {
+          id: buttonEdit.dataset.id,
+          name: buttonEdit.dataset.name,
+          directory: buttonEdit.dataset.directory,
+          server: buttonEdit.dataset.server,
+          path: buttonEdit.dataset.path,
+          type: buttonEdit.dataset.type,
+        };
+
+        document.getElementById('name').value = rowData.name;
+        document.getElementById('directory').value = rowData.directory;
+        document.getElementById('server').value = rowData.server;
+        document.getElementById('path').value = rowData.path;
+        document.getElementById('type').value = rowData.type;
+      }
+
+      if (buttonDelete) {
+        const query = "DELETE FROM sync WHERE name = ?";
+        await window.db.run(query, [buttonDelete.dataset.name]);
+
+        rows = await window.db.all("SELECT * FROM sync");
+        populateTable(rows);
+      }
+    });
+
+    document.getElementById("sync_save").addEventListener("click", async () => {
+      const name = document.getElementById('name');
+      const directory = document.getElementById('directory');
+      const server = document.getElementById('server');
+      const path = document.getElementById('path');
+      const type = document.getElementById('type');
+
+      if (name.value.trim().length > 0 && directory.value.trim().length > 0 && server.value.trim().length > 0 && path.value.trim().length > 0 && type.value.trim().length > 0) {
+        const params = [
+          name.value.trim().toLowerCase(),
+          directory.value.trim().toLowerCase(),
+          server.value.trim().toLowerCase(),
+          path.value.trim().toLowerCase(),
+          type.value.trim().toLowerCase(),
+          // DO UPDATE
+          directory.value.trim().toLowerCase(),
+          server.value.trim().toLowerCase(),
+          path.value.trim().toLowerCase(),
+          type.value.trim().toLowerCase(),
+        ];
+
+        const query = "INSERT INTO sync (name, directory, server, path, type) " +
+            "VALUES (?, ?, ?, ?, ?) " +
+            "ON CONFLICT (name) DO UPDATE SET directory = ?, server = ?, path = ?, type = ?"
+
+        await window.db.run(query, params);
+
+        rows = await window.db.all("SELECT * FROM sync");
+        populateTable(rows);
+
+        name.value = "";
+        directory.value = "";
+        server.value = "";
+        path.value = "";
+        type.value = "";
+      } else {
+        const modalAlert = new bootstrap.Modal('#modal_alert', {
+          keyboard: false
+        });
+        modalAlert.show();
+      }
+    });
+  } catch (error) {
+    console.error("Errore:", error);
+  }
+});
+
+// const populateTable = (data) => {
+//   const tbody = document.querySelector('tbody');
+//   tbody.innerHTML = '';
+//
+//   data.forEach(row => {
+//     const tr = document.createElement('tr');
+//     tr.innerHTML = `
+//       <td>${row.name}</td>
+//       <td>${row.host}</td>
+//       <td>
+//         <button type="button" class="btn btn-outline-warning btn_edit" data-name="${row.name}" data-host="${row.host}" data-username="${row.username}" data-password="${row.password}">
+//           <i class="bi bi-pen"></i>
+//         </button>
+//         <button type="button" class="btn btn-outline-success btn_play" data-name="${row.name}">
+//           <i class="bi bi-play-fill"></i>
+//         </button>
+//         <button type="button" class="btn btn-outline-danger btn_delete" data-name="${row.name}">
+//           <i class="bi bi-trash"></i>
+//         </button>
+//       </td>
+//     `;
+//     tbody.appendChild(tr);
+//   });
+// };
